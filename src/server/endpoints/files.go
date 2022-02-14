f783d92a1cf3b09d0c1d758ee58350e4aa6e55d7
eisandbar <eisandbar@gmail.com>
2022-02-15 02:23:09 +0300

    Boiler plate for files and users handler functions

---
+++ src/server/endpoints/files.go
@@ -0,0 +1,18 @@
+package endpoints
+import (
+
+	"net/http"
+)
+
+
+func FilesPOST(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func FilesDEL(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func FilesGET(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ src/server/endpoints/users.go
@@ -0,0 +1,23 @@
+package endpoints
+
+import (
+
+	"net/http"
+)
+
+
+func UsersPOST(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UsersDEL(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UsersGET(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UsersPUT(w http.ResponseWriter, r *http.Request) {
+
+}
