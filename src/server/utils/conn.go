246e8ba8e1ac555f9cc1a3a3eeba976009a4d77b
eisandbar <eisandbar@gmail.com>
2022-02-16 01:48:19 +0300

    Fixed db connection string

--- src/server/utils/conn.go
+++ src/server/utils/conn.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"fmt"
 )
 
 const (
@@ -14,6 +15,15 @@ var Server_URI = "http://" + Server_HOST + ":" + Server_PORT
 var (
 	db_host = os.Getenv("DB_HOST")
 	db_port = os.Getenv("DB_PORT")
+	USER = os.Getenv("POSTGRES_USER")
+	DB_NAME = os.Getenv("POSTGRES_DB")
+	DB_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
 )
 
-var DB_URI = "http://" + db_host + ":" + db_port
+var DB_URI = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", 
+	db_host,
+	db_port,
+	USER,
+	DB_NAME,
+	DB_PASSWORD,
+)
