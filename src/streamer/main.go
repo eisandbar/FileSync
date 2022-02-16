cd96ca9ed5e4b75bf88a3d986bd8ba076af23ebd
eisandbar <eisandbar@gmail.com>
2022-02-16 17:01:31 +0300

    Basic streamer code. Using a hls media client to test

--- src/streamer/main.go
+++ src/streamer/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-
+	"fmt"
+	"log"
+	"net/http"
+    "github.com/gorilla/mux"
+    "github.com/rs/cors"
 )
 
 func main() {
-	
+	const dir = "files"
+	const port = 8080
+
+	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir(dir)))
+
+	fmt.Printf("Starting server on %v\n", port)
+	log.Printf("Serving %s on HTTP port: %v\n", dir, port)
+
+
+	handler := cors.Default().Handler(router)
+
+	// serve and log errors
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), handler))
 }
